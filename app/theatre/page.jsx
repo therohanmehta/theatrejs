b1b075d9ed04cd1a1375dd9269b562ce79050502
therohanmehta <rohandhn786@gmail.com>
2024-09-22 15:37:19 +0530

    animation fixes

--- app/theatre/page.jsx
+++ app/theatre/page.jsx
@@ -46,13 +46,13 @@ function Theatre() {
           <Canvas
             gl={{ preserveDrawingBuffer: true }}
             style={{ height: "100vh", width: "100%" }}
-            className="bg-red-400"
+            className=""
             shadows
-            camera={{ position: [1, 1, 40], fov: 30 }}
+            camera={{ position: [0, 0, 10], fov: 75 }}
           >
             <SheetProvider sheet={rohanSheet}>
               <Environment preset="sunset" />
-              <e.group theatreKey="rohan" position={[0, -20, -10]} scale={[15, 15, 15]} rotation={[Math.PI / 2, 0, 0]}>
+              <e.group theatreKey="rohan" position={[0, -20, -10]} scale={[3, 3, 3]} rotation={[Math.PI / 2, 0, 0]}>
                 <RohanModel />
               </e.group>
             </SheetProvider>
@@ -65,126 +65,41 @@ function Theatre() {
 
 function RohanModel(props) {
   const modelRef = useRef();
-  const { animation, cursorFollow, scrollAnimation } = props;
 
-  const { nodes, materials } = useGLTF("/models/model.glb");
+  // Load GLTF model
+  const { scene } = useGLTF("/models/model.glb");
 
+  // Load FBX animations
   const { animations: WalkingAnimation } = useFBX("/animations/Walking.fbx");
-  const { animations: PushupAnimation } = useFBX("/animations/PushUp.fbx");
-  const { animations: SleepingAnimation } = useFBX("/animations/Sleeping.fbx");
-  const { animations: TextingAnimation } = useFBX("/animations/Texting.fbx");
-  const { animations: BurpeeAnimation } = useFBX("/animations/Burpee.fbx");
 
+  // Apply animation names (optional if not already named in the FBX)
   WalkingAnimation[0].name = "Walk";
-  PushupAnimation[0].name = "Pushup";
-  SleepingAnimation[0].name = "Sleeping";
-  TextingAnimation[0].name = "Texting";
-  BurpeeAnimation[0].name = "Burpee";
 
-  const { actions } = useAnimations(
-    [WalkingAnimation[0], PushupAnimation[0], SleepingAnimation[0], TextingAnimation[0], BurpeeAnimation[0]],
-    modelRef
-  );
+  // Use the animation
+  const { actions } = useAnimations(WalkingAnimation, modelRef);
 
+  // Start the walking animation on mount
   useEffect(() => {
-    actions["Walk"].play();
-  }, [animation, scrollAnimation, cursorFollow, actions]);
+    if (actions["Walk"]) {
+      actions["Walk"].reset().play();
+    }
 
-  // useEffect(() => {
-  //   const handleScroll = debounce(() => {
-  //     actions["Burpee"].stop();
-  //   }, 100);
-
-  //   const handleScrollStop = debounce(() => {
-  //     actions["Burpee"].play();
-  //   }, 200);
-
-  //   window.addEventListener("scroll", handleScroll);
-  //   window.addEventListener("scroll", handleScrollStop);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //     window.removeEventListener("scroll", handleScrollStop);
-  //     handleScroll.cancel();
-  //     handleScrollStop.cancel();
-  //   };
-  // }, [actions]);
+    // Cleanup on unmount
+    return () => {
+      actions["Walk"]?.stop();
+    };
+  }, [actions]);
 
   return (
     <group {...props} ref={modelRef} dispose={null}>
-      <group>
-        <primitive object={nodes.Hips} />
-        <skinnedMesh
-          name="EyeLeft"
-          geometry={nodes.EyeLeft.geometry}
-          material={materials.Wolf3D_Eye}
-          skeleton={nodes.EyeLeft.skeleton}
-          morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
-          morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
-        />
-        <skinnedMesh
-          name="EyeRight"
-          geometry={nodes.EyeRight.geometry}
-          material={materials.Wolf3D_Eye}
-          skeleton={nodes.EyeRight.skeleton}
-          morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
-          morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
-        />
-        <skinnedMesh
-          name="Wolf3D_Head"
-          geometry={nodes.Wolf3D_Head.geometry}
-          material={materials.Wolf3D_Skin}
-          skeleton={nodes.Wolf3D_Head.skeleton}
-          morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
-          morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
-        />
-        <skinnedMesh
-          name="Wolf3D_Teeth"
-          geometry={nodes.Wolf3D_Teeth.geometry}
-          material={materials.Wolf3D_Teeth}
-          skeleton={nodes.Wolf3D_Teeth.skeleton}
-          morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
-          morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
-        />
-        <skinnedMesh geometry={nodes.Wolf3D_Hair.geometry} material={materials.Wolf3D_Hair} skeleton={nodes.Wolf3D_Hair.skeleton} />
-        <skinnedMesh geometry={nodes.Wolf3D_Glasses.geometry} material={materials.Wolf3D_Glasses} skeleton={nodes.Wolf3D_Glasses.skeleton} />
-        <skinnedMesh
-          name="Wolf3D_Outfit_Top"
-          geometry={nodes.Wolf3D_Outfit_Top.geometry}
-          material={materials.Wolf3D_Outfit_Top}
-          skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
-          morphTargetDictionary={nodes.Wolf3D_Outfit_Top.morphTargetDictionary}
-          morphTargetInfluences={nodes.Wolf3D_Outfit_Top.morphTargetInfluences}
-        />
-        <skinnedMesh
-          name="Wolf3D_Outfit_Bottom"
-          geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
-          material={materials.Wolf3D_Outfit_Bottom}
-          skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
-          morphTargetDictionary={nodes.Wolf3D_Outfit_Bottom.morphTargetDictionary}
-          morphTargetInfluences={nodes.Wolf3D_Outfit_Bottom.morphTargetInfluences}
-        />
-        <skinnedMesh
-          name="Wolf3D_Outfit_Footwear"
-          geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
-          material={materials.Wolf3D_Outfit_Footwear}
-          skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
-          morphTargetDictionary={nodes.Wolf3D_Outfit_Footwear.morphTargetDictionary}
-          morphTargetInfluences={nodes.Wolf3D_Outfit_Footwear.morphTargetInfluences}
-        />
-        <skinnedMesh
-          name="Wolf3D_Body"
-          geometry={nodes.Wolf3D_Body.geometry}
-          material={materials.Wolf3D_Body}
-          skeleton={nodes.Wolf3D_Body.skeleton}
-          morphTargetDictionary={nodes.Wolf3D_Body.morphTargetDictionary}
-          morphTargetInfluences={nodes.Wolf3D_Body.morphTargetInfluences}
-        />
-      </group>
+      <primitive object={scene} />
     </group>
   );
 }
 
+// Preload the GLTF model to avoid reloading it later
 useGLTF.preload("/models/model.glb");
+
+// useGLTF.preload("/models/model.glb");
 
 export default Theatre;
